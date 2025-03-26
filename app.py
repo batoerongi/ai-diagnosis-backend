c24dd30ab1f2120caf2338110cbdc528c61861af
batoerongi <batoerongi@gmail.com>
2025-03-26 22:02:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def diagnose():
         return jsonify({'status': 'error', 'message': f'Gagal menganalisa: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
+
